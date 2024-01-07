deec2b883f638820e5573d6403ec346044b906ca
kelvin kimani <kevohkyme35@gmail.com>
2024-01-07 21:15:23 +0300

    Host on Render

---
+++ routes/places-routes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const checkAuth = require("../middlewares/check-auth");
+const { check } = require("express-validator");
+const placeControllers = require("../controllers/places-controller");
+const fileUpload = require("../middlewares/file-upload");
+const router = express.Router();
+
+router.get("/:pid", placeControllers.getPlaceById);
+
+router.get("/user/:uid", placeControllers.getPlacesByUserId);
+
+router.use(checkAuth);
+router.post(
+  "/",
+  fileUpload.single("image"),
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placeControllers.createPlace
+);
+
+router.patch(
+  "/:pid",
+  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],
+  placeControllers.updatePlace
+);
+
+router.delete("/:pid", placeControllers.deletePlace);
+
+module.exports = router;
---
+++ util/location.js
@@ -0,0 +1,10 @@
+//const axios = require("axios")
+
+const getCoordsForAddress = () => {
+  return {
+    lng: 40.4547346,
+    lat: -73.4775887,
+  };
+};
+
+module.exports = getCoordsForAddress;
